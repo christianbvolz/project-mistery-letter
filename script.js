ff25d2c37bf3b45133a97ee2977baa939c4d046b
Christianbv <christianbvolz@gmail.com>
2021-07-15 23:49:54 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function geraCarta() {
+function geraCarta(value) {
   document.getElementById('envelope').removeChild(document.getElementById('carta-gerada'));
-  const input = document.getElementById('carta-texto').value.split(' ');
+  const input = value.split(' ');
   const envelope = document.getElementById('envelope');
   const paragraph = document.createElement('p');
   paragraph.id = 'carta-gerada';
@@ -12,11 +12,29 @@ function geraCarta() {
   }
 }
 
+function validaCarta() {
+  let valueInput = document.getElementById('carta-texto').value;
+  let checkInput = false;
+  const input = valueInput;
+  if (valueInput) {
+    valueInput = valueInput.split(' ').forEach(function (element, index, array) {
+      if (element) {
+        checkInput = true;
+      }
+    });
+  }
+  if (checkInput) {
+    console.log('eeeeee');
+    geraCarta(input);
+  } else {
+    document.getElementById('carta-gerada').innerHTML = 'Por favor, digite o conteúdo da carta.';
+    document.getElementById('carta-texto').value = '';
+  }
+}
 
-document.getElementById('criar-carta').addEventListener('click', geraCarta);
-
+document.getElementById('criar-carta').addEventListener('click', validaCarta);
 document.getElementById('carta-texto').addEventListener('keypress', (event) => {
   if (event.keyCode === 13) {
-    geraCarta();
+    validaCarta();
   }
 });
